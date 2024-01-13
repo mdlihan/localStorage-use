8385533d45215d767f4ea02c7f64f4c0384ee378
MD Lihan <147899596+mdlihan@users.noreply.github.com>
2024-01-12 20:12:02 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+localStorage.setItem('name','lihan');
+localStorage.setItem('year','2005');
+
+// const n1 = localStorage.getItem("name")
+// console.log(n1);
+
+// const n2 = localStorage.getItem("year")
+// console.log(n2);
+
+// console.log(localStorage.removeItem("name"));
+
+
+// const data=['Bangladesh','Saturia','Manikgonj','Dhaka'];
+// localStorage.setItem("alldata",JSON.stringify(data));
+
+// console.log(JSON.parse(localStorage.getItem("alldata")));
